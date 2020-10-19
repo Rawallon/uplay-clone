2052c60dc1477d5efba6ecefe177e98768bf49e4
OTalDoDante <rawallon@gmail.com>
2020-10-19 19:17:46 -0300

    first commit

---
+++ src/global/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+import UbiRegSansEOT from '../assets/font/ubisoftsans-regular.eot'
+import UbiRegSansWOFF from '../assets/font/ubisoftsans-regular.woff'
+
+import UbiBoldSansWOFF from '../assets/font/ubisoftsans-bold.woff'
+import UbiBoldSansEOT from '../assets/font/ubisoftsans-bold.eot'
+
+export const Globals = createGlobalStyle`
+/* FONT FROM: https://www.fontzip.com/ubisoft-sans */
+    @font-face {
+        font-family: 'UbisoftSans';
+        src: url(${UbiRegSansEOT}) format('eot'),
+        url(${UbiRegSansWOFF}) format('woff');
+        font-weight: normal;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'UbisoftSansBold';
+        src: url(${UbiBoldSansEOT}) format('eot'),
+        url(${UbiBoldSansWOFF}) format('woff');
+        font-weight: bold;
+        font-style: normal;
+    }
+    /* FONT FROM https://www.fontzip.com/ubisoft-sans */
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    border: 0;
+    box-sizing: border-box;
+  }
+  html, body, :root {
+    font-family: 'ubisoftsans','Poppins', Arial, Helvetica, sans-serif
+  }
+`;
