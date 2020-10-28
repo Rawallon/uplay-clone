4f73daa2a93e5a40094b5c558aa6d72dcdf67a3d
OTalDoDante <rawallon@gmail.com>
2020-10-28 17:23:06 -0300

    Updates

---
+++ precache-manifest.2b12dff76e7b40a1bb978cc890f81796.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f741525b5b29f7f2fc4cf7a390ecd1fd",
+    "url": "/uplay-clone/index.html"
+  },
+  {
+    "revision": "b4c993984e9dadc66cc4",
+    "url": "/uplay-clone/static/js/2.91a93cca.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/uplay-clone/static/js/2.91a93cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81df66a1a05e8763d7e7",
+    "url": "/uplay-clone/static/js/main.df29a86d.chunk.js"
+  },
+  {
+    "revision": "199cff8d8184bd390625",
+    "url": "/uplay-clone/static/js/runtime-main.762fe6aa.js"
+  },
+  {
+    "revision": "eca108dbd05572678a3d14900ddc6afe",
+    "url": "/uplay-clone/static/media/acblackflag.eca108db.png"
+  },
+  {
+    "revision": "ac74fe4baa3a53226656cb2aca2c35e8",
+    "url": "/uplay-clone/static/media/achiev.ac74fe4b.webp"
+  },
+  {
+    "revision": "4dc0d17ecef74a43608de74b674ba7fe",
+    "url": "/uplay-clone/static/media/achiev1.4dc0d17e.webp"
+  },
+  {
+    "revision": "706f01ca28d329ce0a443d801d68d984",
+    "url": "/uplay-clone/static/media/badge-placeholder.706f01ca.svg"
+  },
+  {
+    "revision": "698367fcfcac4436db868cdb14858a0a",
+    "url": "/uplay-clone/static/media/banner-right.698367fc.webp"
+  },
+  {
+    "revision": "fe0b2a5ac15691a15c57afb16b6f4031",
+    "url": "/uplay-clone/static/media/big-logo.fe0b2a5a.svg"
+  },
+  {
+    "revision": "15502a55c497c40f22e9276d34742385",
+    "url": "/uplay-clone/static/media/club-header.15502a55.jpg"
+  },
+  {
+    "revision": "cda8a81994093dad9cffc360a25776b0",
+    "url": "/uplay-clone/static/media/coin.cda8a819.svg"
+  },
+  {
+    "revision": "f2af70cd010b5adf8325a5bde84ab3ef",
+    "url": "/uplay-clone/static/media/mini-logo.f2af70cd.svg"
+  },
+  {
+    "revision": "2e6e061043c3dd51126c9e934cfe3c33",
+    "url": "/uplay-clone/static/media/profile.2e6e0610.jpg"
+  },
+  {
+    "revision": "3dcf33b283b732aaaf7e64eb2b750a31",
+    "url": "/uplay-clone/static/media/rayman.3dcf33b2.png"
+  },
+  {
+    "revision": "655b5286ad19ee9674f84b01cda1267a",
+    "url": "/uplay-clone/static/media/tcgrwildlands.655b5286.png"
+  },
+  {
+    "revision": "77a3c9c5b226c2e40d3f40fb55a37a01",
+    "url": "/uplay-clone/static/media/tcrs.77a3c9c5.png"
+  },
+  {
+    "revision": "857c206159bf7ef69f01bdcf50ce7bc6",
+    "url": "/uplay-clone/static/media/tdiv2.857c2061.png"
+  },
+  {
+    "revision": "234c04b8b057e165d921c4363b56249d",
+    "url": "/uplay-clone/static/media/ubisoftsans-bold.234c04b8.woff"
+  },
+  {
+    "revision": "5969ac44ddf67bb1ce9d4afe2f01a649",
+    "url": "/uplay-clone/static/media/ubisoftsans-bold.5969ac44.eot"
+  },
+  {
+    "revision": "58bb44fcceb3994b768bc39b3974895e",
+    "url": "/uplay-clone/static/media/ubisoftsans-regular.58bb44fc.eot"
+  },
+  {
+    "revision": "8ae7a4413280e96317f101e244008d75",
+    "url": "/uplay-clone/static/media/ubisoftsans-regular.8ae7a441.woff"
+  }
+]);
